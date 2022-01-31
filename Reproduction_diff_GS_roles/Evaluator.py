9eaa56acc8316415036f5ca25c91d483773a5a29
毕夏 <978260577@qq.com>
2022-01-31 16:08:58 +0800

    Fix bugs

--- Reproduction_diff_GS_roles/Evaluator.py
+++ Reproduction_diff_GS_roles/Evaluator.py
@@ -62,135 +62,6 @@ class Evaluator:
                     data.append(temp)
         return data
 
-    def individual_evaluate(self, label=None):
-        """
-        Only for the individual search
-        :param label: the curve label, mainly for the agent name (e.g., T shape 22, T shape 41)
-        :return: the figure
-        """
-        data_files = self.load_files_under_folder()
-        data = self.load_data_from_files(files_path=data_files)
-        # individual level comparison
-        figure, axis = plt.subplots()
-        for name, each_data in zip(label, data):
-            # print(len(each_data), len(each_data[0]), len(each_data[0][0]))
-            # landscape_iteraton=5;     agent_iteration = 200; search_iteration = 200
-            axis.plot(np.mean(np.mean(np.array(each_data), axis=0), axis=0), label="{}".format(name))
-
-        axis.set_xlabel('Search iteration')  # Add an x-label to the axes.
-        axis.set_ylabel('Average fitness')  # Add a y-label to the axes.
-
-        flag = 0
-        for each in list(data_files[0]):
-            if (each == "_") & (flag < 2):
-                flag += 1
-            if flag == 2:
-                self.title += each
-
-        axis.set_title(self.title)  # Add a title to the axes.
-        plt.legend()
-        output = self.output_path + "\\" + self.title + ".png"
-        plt.savefig(output)  # save the figure before plt.show(). Otherwise, there is no information.
-        plt.show()
-
-    def team_evaluate(self, label=None):
-        """
-        For team search, but also for one dimension figure
-        either fixed team type + dynamic K or fixed K + dynamic team types
-        :param label: the dimension that changes (e.g., either K values or team types)-> added into legend
-        :return: the figure in the same file folder as data
-        """
-        data_files = []
-        for root, dirs, files in os.walk(self.data_path):
-            for ech_file in files:
-                data_files.append(root + '\\' + ech_file)
-        data = []
-        for each_file in data_files:
-            if ".png" not in each_file:
-                with open(each_file,'rb') as infile:
-                    print(each_file)
-                    temp = pickle.load(infile)
-                    data.append(temp)
-        print("file number, landscape loop, agent loop. searh loop: ", np.array(data).shape)
-
-        # individual level comparison
-        figure, axis = plt.subplots()
-        for name, each_data in zip(label, data):
-            # print(len(each_data), len(each_data[0]), len(each_data[0][0]))
-            # landscape_iteraton=5;     agent_iteration = 200; search_iteration = 200
-            axis.plot(np.mean(np.mean(np.array(each_data), axis=0), axis=0), label="{}".format(name))
-
-        axis.set_xlabel('Search iteration')  # Add an x-label to the axes.
-        axis.set_ylabel('Average fitness')  # Add a y-label to the axes.
-        axis.set_title(self.title)  # Add a title to the axes.
-        plt.legend()
-        output = self.output_path + "\\" + self.title + ".png"
-        plt.savefig(output)  # save the figure before plt.show(). Otherwise, there is no information.
-        plt.show()
-
-    def team_evaluate_2by2_matrix(self, label=None, figure_x=None, figure_y=None, re_x_rule_list=None):
-        """
-        For team search, but also for one dimension figure
-        either fixed team type + dynamic K or fixed K + dynamic team types
-        :param label: the dimension that changes (e.g., either K values or team types)
-        :return: the figure in the same file folder as data
-        """
-        # here data_path could be a list-> because we have two dimensions (team type + K)
-        # or three dimensions (team type + K + N)
-        # if figure_x:
-        #     if figure_x != len(self.data_path[0]):
-        #         raise ValueError("{0} folders doesn't fit figure_x = {1}".format(len(self.data_path), figure_x))
-        # if figure_y:
-        #     if figure_y != len(self.data_path):
-        #         raise ValueError("{0} folders doesn't fit figure_y = {1}".format(len(self.data_path[0]), figure_y))
-
-        y_parent_folder = []
-        subplot_title = []
-        for each_x_rule in re_x_rule_list:
-            x_parent_folder = []
-            for root, dirs, files in os.walk(self.data_path):
-                for each_dir in dirs:
-                    if re.search(pattern=each_x_rule, string=each_dir):
-                        x_parent_folder.append(root +"\\"+ each_dir)
-                        subplot_title.append(re.search(pattern=each_x_rule, string=each_dir).group(1))
-            y_parent_folder.append(x_parent_folder)
-
-        # load the data
-        data = []
-        for y_index in range(figure_y):
-            y_folder_list = y_parent_folder[y_index]
-            data_x = []
-            for x_index in range(figure_x):
-                x_folder_list = y_folder_list[x_index]
-                print("**************")
-                for root, dirs, files in os.walk(x_folder_list):
-                    for each_file in files:
-                        if ".png" not in each_file:
-                            print("Check the order of team type (the label !!)", each_file)
-                            with open(root+"\\" + each_file, 'rb') as infile:
-                                temp = pickle.load(infile)
-                                data_x.append(temp)
-            data.append(data_x)
-
-
-
-        print("len(label)", len(label))
-        # individual level comparison
-        figure = plt.figure()
-        for y, each_row_data in zip(range(figure_y), data): # within-figure: different team types
-            for x, each_column_data in zip(range(figure_x), each_row_data):
-                ax = figure.add_subplot(y+1, figure_x, x+1)
-                ax.title.set_text("{0}".format(subplot_title[x]))
-                for z, each_curve in zip(range(len(label)), each_column_data):
-                    ax.plot(np.mean(np.mean(np.array(each_column_data), axis=0), axis=0), label="{}".format(label[z]))
-        ax.set_xlabel('Search iteration')  # Add an x-label to the axes.
-        ax.set_ylabel('Average fitness')  # Add a y-label to the axes.
-        ax.set_title(self.title)  # Add a title to the whole figure
-        plt.legend()
-        output = self.output_path + "\\" + self.title + ".png"
-        plt.savefig(output)  # save the figure before plt.show(). Otherwise, there is no information.
-        plt.show()
-
     def f_test(self, baseline=None, showline=None):
         """
         Compare everay two dataset to see whether there is a significant difference and how much
@@ -213,31 +84,6 @@ class Evaluator:
         else:
             star = '.'
         return f, p, star
-
-    def final_fitness_order(self):
-        """
-        The final converge is the performance of each agent-task combination
-        :return: the converge figure
-        """
-        pass
-
-    def performance_polyline_across_type_k(self):
-        """
-        Line graph across different agent or team type and landscape ruggedness
-        :return: polyline figure
-        """
-        agent_folders = self.load_folders_under_parent_folder(parent_folder=self.data_path)
-        data_overall = []
-        for agent_level_folder in agent_folders:
-            ruggedness_level_folder = self.load_folders_under_parent_folder(parent_folder=agent_level_folder)
-            data_agent = []
-            for each_folder in ruggedness_level_folder:
-                data_files = self.load_files_under_folder(each_folder)
-                data_ruggedness = []
-                for each_file in data_files:
-                    data_ruggedness = self.load_data_from_files(each_file)
-                data_agent.append(data_ruggedness)
-            data_overall.append(data_agent)
 
     def convergence_evaluation(self, label_list=None, show_variance=False, select_list=None):
         folders_list = self.load_folders_under_parent_folder(parent_folder=self.data_path)
@@ -319,21 +165,22 @@ class Evaluator:
 if __name__ == '__main__':
     # Test Example
     # Convergence evaluation
-    # parent_folder = r"C:\Python_Workplace\hpc-0126\nk\Influential\convergence"
-    # output_path = parent_folder
-    # evaluator =Evaluator(title="GS Convergence in Influential IM N10", data_path=parent_folder, output_path=output_path)
+    parent_folder = r"C:\Python_Workplace\hpc-0131\k44\Convergence"
+    output_path = parent_folder
+    evaluator =Evaluator(title="GS Convergence in IM N10", data_path=parent_folder, output_path=output_path)
     # team_name = ["G", "S", "T43", "T62"]
-    # # team_name = ["G", "S"]
-    # # evaluator.convergence_evaluation(label_list=team_name)
+    team_name = ["S", "T", "T"]
+    # evaluator.convergence_evaluation(label_list=team_name)
     # evaluator.convergence_evaluation(label_list=team_name, select_list=[0,1], show_variance=True)
+    evaluator.individual_evaluate()
 
 
     # match evaluation
-    parent_folder = r"C:\Python_Workplace\hpc-0126\nk\Factor\column_match"
-    output_path = parent_folder
-    evaluator = Evaluator(title="Individual Match in Factor IM N10", data_path=parent_folder, output_path=output_path)
-    team_name = ["G", "S", "T43", "T62"]
-    # team_name = ["G", "S"]
-    # evaluator.match_evaluation(label_list=team_name)
-    evaluator.match_evaluation(label_list=team_name, select_list=[3], show_variance=True)
+    # parent_folder = r"C:\Python_Workplace\hpc-0126\nk\Factor\column_match"
+    # output_path = parent_folder
+    # evaluator = Evaluator(title="Individual Match in Factor IM N10", data_path=parent_folder, output_path=output_path)
+    # team_name = ["G", "S", "T43", "T62"]
+    # # team_name = ["G", "S"]
+    # # evaluator.match_evaluation(label_list=team_name)
+    # evaluator.match_evaluation(label_list=team_name, select_list=[3], show_variance=True)
 
