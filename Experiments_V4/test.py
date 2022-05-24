a63121cee0a78f3ad48c34b6abdabb07d10dda9f
毕夏 <978260577@qq.com>
2022-05-24 12:54:29 +0800

    Experiment V4: Only run the multi-interaction Partition the task into sub-task but under the same set-ups

--- Experiments_V4/test.py
+++ Experiments_V4/test.py
@@ -4,4 +4,12 @@
 # @FileName: test.py
 # @Software  : PyCharm
 # Observing PEP 8 coding style
-print("Test")
+import pickle
+import numpy as np
+
+file = r'C:\Python_Workplace\hpc-0522\Pilot_Test\1Average_N6_K0_E8_Self-interested_SS0_GG0_F0_Prop0.5_Q0_O0_'
+with open(file, 'rb') as in_file:
+    IM = pickle.load(in_file)
+    for each in np.array(IM):
+        print(each)
+print(len(IM))
