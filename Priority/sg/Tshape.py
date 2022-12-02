6c648a8a5eee4ea628a3b3ac444da99c4a8a3417
[Junee] <[978260577@qq.com]>
2022-12-03 01:31:46 +0800

    # Need to follow the four/three regimes from Gavetti 2005's paper to design the coordination patterns. ---Autonomous pattern ---Priority pattern

--- Priority/sg/Tshape.py
+++ Priority/sg/Tshape.py
@@ -112,6 +112,26 @@ class Tshape:
             self.cog_fitness = next_cog_fitness
             self.fitness, self.potential_fitness = self.landscape.query_cog_fitness_full(cog_state=self.cog_state)
 
+    def priority_search(self, co_state=None, co_expertise_domain=None):
+        # learning from coupled agent
+        next_cog_state = self.cog_state.copy()
+        for index in range(self.N):
+            if index in co_expertise_domain:
+                next_cog_state[index] = co_state[index]
+            else:
+                pass
+        index = np.random.choice(self.expertise_domain)  # only select from the expertise domain,
+        # thus will not change the unknown domain
+        space = ["0", "1", "2", "3"]
+        space.remove(self.state[index])
+        next_cog_state[index] = np.random.choice(space)
+        next_cog_fitness = self.landscape.query_cog_fitness_partial(cog_state=next_cog_state,
+                                                                    expertise_domain=self.expertise_domain)
+        if next_cog_fitness > self.cog_fitness:
+            self.cog_state = next_cog_state
+            self.cog_fitness = next_cog_fitness
+            self.fitness, self.potential_fitness = self.landscape.query_cog_fitness_full(cog_state=self.cog_state)
+
     def state_2_cog_state(self, state=None):
         cog_state = self.state.copy()
         for index, bit_value in enumerate(state):
