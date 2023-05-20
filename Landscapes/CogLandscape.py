4e605105fe4ab8b39474f4f8543ce91e271312e6
毕夏 <978260577@qq.com>
2023-05-20 17:19:35 +0800

    Volidate the Multi-State Landscape -- is there any inherent differences between binary and polynary landscape (e.g., distribution, variance, quantiles, max-min, etc)? -> Yes, G landscape is more smooth and concentrated (average pooling concentrate the distribution) -- will full S outperform full G for certainty? -> Full G > Full S -- will G and S with the same amount of knowledge perform differently? -> Partial G < Partial S -- will the performance always increase with increasing knowledge (tunable elements)
    -> These conclusions are based on the average pooling. In other words, the payoff is randomly assigned (expectation is the same), rather than damage if decisions are not made. The difference is the variance, randomly generated values lead to higher variance, whereas average pooling lead to smaller variance. Yet, we believe smaller variance is more reasonable for modeling generalists and shallow understandings. This average pooling is coherent with the NK model, recalling the integration from FC into fitness; More importantly, normal distribution is common adopted in social phenomenon. This tradition are strongly built on normal distribution, unless strong reasons are provided (e.g., Levinthal's (2022) Organizational Task Environments). In this paper, we stay in the tradition of normal distribution. One can extend our model to explore the boundaries of this paper.
    -> If the assumptions are challenged, the conclusion may be different-> Partial knowledge is always harmful. Yet, there are many cases where one can stay ambiguious; In fact, this also resonate with the focusing problem-solving. In fact, if we release the scope constrains, G and S will continuous to improve their performance. Thus, we are not arguing that increasing knowledge is harmful. Instead, we are saying that tuning all the elements at the same time is harmful. Simplified caricatures help the search to jump out of the local traps. Thus, the question comes as to the trade-off between focusing and divergency. How many elements should generalists and specialists handles?
    
    --recall the unknown domain shelter "*"
    -- If not applicable, may roll back to default value setups
    --Design separate CogLandscape class
    --Comparing the search trajectories among cog, ave, max, and min fitness values

---
+++ Landscapes/CogLandscape.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# @Time     : 5/11/2023 15:26
+# @Author   : Junyi
+# @FileName: Cog_landscape.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+from collections import defaultdict
+from itertools import product
+import numpy as np
+
+
+class CogLandscape:
+    def __init__(self, landscape=None, expertise_domain=None, expertise_representation=None,
+                 norm="MaxMin", collaborator="None"):
+        self.landscape = landscape
+        self.expertise_domain = expertise_domain
+        self.expertise_representation = expertise_representation
+        self.N = self.landscape.N
+        self.K = self.landscape.K
+        self.state_num = self.landscape.state_num
+        self.IM, self.dependency_map = self.landscape.IM, self.landscape.dependency_map
+        self.FC = self.landscape.FC
+        self.cache = {}  # state string to partial fitness: state_num ^ N: [1]
+        self.max_normalizer = 1
+        self.min_normalizer = 0
+        self.norm = norm
+        self.collaborator = collaborator
+        self.initialize()
+
+    def describe(self):
+        print("*********CogLandScape information********* ")
+        print("LandScape shape of (N={0}, K={1}, state number={2})".format(self.N, self.K, self.state_num))
+        print("Influential matrix: \n", self.IM)
+        print("Influential dependency map: ", self.dependency_map)
+        print("********************************")
+
+    def calculate_cog_fitness(self, cog_state=None):
+        partial_fitness_alternatives = []
+        alternatives = self.cog_state_alternatives(cog_state=cog_state)  # get back to the finest level
+        # so that we can calculate it using FC, which is all at the finest level (i.e., 0, 1, 2, 3)
+        for state in alternatives:
+            partial_FC_across_bits = []
+            for index in range(len(state)):
+                if index not in self.expertise_domain:
+                    continue
+                dependency = self.dependency_map[index]
+                bit_index = "".join([str(state[j]) for j in dependency])
+                bit_index = str(state[index]) + bit_index
+                FC_index = int(bit_index, self.state_num)
+                partial_FC_across_bits.append(self.FC[index][FC_index])
+            partial_fitness_state = sum(partial_FC_across_bits) / len(self.expertise_domain)
+            partial_fitness_alternatives.append(partial_fitness_state)
+        return sum(partial_fitness_alternatives) / len(partial_fitness_alternatives)
+
+    def store_cache(self):
+        # Cartesian products within knowledge scope (not combinations or permutations)
+        known_products = list(product(self.expertise_representation, repeat=len(self.expertise_domain)))
+        if self.collaborator == "Generalist":
+            all_representation = ["A", "B", "*"]
+        elif self.collaborator == "Specialist":
+            all_representation = ["0", "1", "2", "3", "*"]
+        else:
+            all_representation = ["*"]
+        if len(self.expertise_domain) < self.N:
+            # Cartesian products outside knowledge scope (not combinations or permutations)
+            unknown_products = list(product(all_representation, repeat=self.N - len(self.expertise_domain)))
+            all_cog_states = []
+            unknown_domain = [i for i in range(self.N) if i not in self.expertise_domain]
+            for each_known in known_products:
+                for each_unknown in unknown_products:
+                    combined_list = np.zeros(self.N, dtype=object)
+                    combined_list[self.expertise_domain] = each_known
+                    combined_list[unknown_domain] = each_unknown
+                    all_cog_states.append(combined_list)
+        else:
+            all_cog_states = known_products
+        if len(all_cog_states) != len(self.expertise_representation) ** len(self.expertise_domain) * \
+                len(all_representation) ** (self.N - len(self.expertise_domain)):
+            raise ValueError("All State is Problematic")
+        for cog_state in all_cog_states:
+            bits = ''.join(cog_state)
+            self.cache[bits] = self.calculate_cog_fitness(cog_state)
+
+    def initialize(self):
+        """
+        Cache the fitness value
+        :param norm: normalization
+        :return: fitness cache
+        """
+        self.store_cache()
+        self.max_normalizer = max(self.cache.values())
+        self.min_normalizer = min(self.cache.values())
+        # normalization
+        if self.norm == "MaxMin":
+            for k in self.cache.keys():
+                self.cache[k] = (self.cache[k] - self.min_normalizer) / (self.max_normalizer - self.min_normalizer)
+        elif self.norm == "Max":
+            for k in self.cache.keys():
+                self.cache[k] = self.cache[k] / self.max_normalizer
+
+    def query_cog_fitness(self, cog_state=None):
+        return self.cache["".join(cog_state)]
+
+    def cog_state_alternatives(self, cog_state=None):
+        alternative_pool = []
+        for bit in cog_state:
+            if bit in ["0", "1", "2", "3"]:
+                alternative_pool.append(bit)
+            elif bit == "A":
+                alternative_pool.append(["0", "1"])
+            elif bit == "B":
+                alternative_pool.append(["2", "3"])
+            elif bit == "*":
+                alternative_pool.append(["0", "1", "2", "3"])
+            else:
+                raise ValueError("Unsupported bit value: ", bit)
+        return [i for i in product(*alternative_pool)]
+
+
+if __name__ == '__main__':
+    # Test Example
+    import time
+    from Landscape import Landscape
+    from Generalist import Generalist
+    from Specialist import Specialist
+    t0 = time.time()
+    N = 9
+    K = 7
+    state_num = 4
+    expertise_amount = 16
+    landscape = Landscape(N=N, K=K, state_num=state_num)
+    specialist = Specialist(N=N, landscape=landscape, state_num=state_num, expertise_amount=expertise_amount)
+    specialist.describe()
+    cog_landscape = CogLandscape(landscape=landscape, expertise_domain=specialist.expertise_domain, expertise_representation=specialist.expertise_representation)
+    specialist.cog_landscape = cog_landscape
+    specialist.update_cog_fitness()
+    specialist.describe()
+    cog_landscape.describe()
+    t1 = time.time()
+    print(time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
+
+
+
+
