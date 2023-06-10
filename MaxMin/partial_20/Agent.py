0a8d476ae0b79ca348779d1d40aeb9c967dbd69e
毕夏 <978260577@qq.com>
2023-06-10 21:05:35 +0800

    1. Simplify the model, Only two class: --Agent class for G/S/T --Landscape class for 4-state landscape
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --adopt the default element design, resonating with the mindset and cognitive fixation; perhaps removed.
    --adopt the shelter "*" element design, resonating with the myopia induced ambiguity.
    
    3. Introduce the different sub-area in the landscape, removed.
    
    4. Adjust the ambiguity design
    --the average pooling algorithm now is much more informative and consistent with neighborhood perception.

--- MaxMin/partial_20/Agent.py
+++ MaxMin/partial_20/Agent.py
@@ -59,7 +59,8 @@ class Agent:
 
     def search(self):
         next_state = self.state.copy()
-        index = np.random.choice(self.generalist_domain + self.specialist_domain)
+        # index = np.random.choice(self.generalist_domain + self.specialist_domain)
+        index = np.random.choice(range(self.N))
         free_space = ["0", "1", "2", "3"]
         free_space.remove(next_state[index])
         next_state[index] = np.random.choice(free_space)
@@ -78,7 +79,9 @@ class Agent:
                 self.cog_cache[perception] = next_cog_fitness
             else:
                 next_cog_fitness = self.cog_cache[perception]
-        if next_cog_fitness > self.cog_fitness:
+        if next_cog_fitness >= self.cog_fitness:
+            if self.landscape.query_fitness(state=self.state) < self.fitness:
+                print("Back", self.state, self.cog_state, self.fitness, self.cog_fitness)
             self.state = next_state
             self.cog_state = next_cog_state
             self.cog_fitness = next_cog_fitness
@@ -94,11 +97,11 @@ class Agent:
                     cog_state[index] = "B"
                 else:
                     raise ValueError("Only support for state number = 4")
-            # elif index in self.specialist_domain:
-            #     pass
-            else:
+            elif index in self.specialist_domain:
                 pass
-                # cog_state[index] = "*"
+            else:
+                # pass
+                cog_state[index] = "*"
         return cog_state
 
     # def cog_state_2_state(self, cog_state=None):
