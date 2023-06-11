fd08996435cf409af93029cfdb843554ab2c5257
毕夏 <978260577@qq.com>
2023-06-11 19:45:35 +0800

    1. Simplify the model, Only two class: --Agent class for G/S/T --Landscape class for 4-state landscape
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --adopt the default element design, resonating with the mindset and cognitive fixation; perhaps removed.
    --adopt the shelter "*" element design, resonating with the myopia induced ambiguity.
    
    3. Introduce the different sub-area in the landscape, removed.
    
    4. Adjust the ambiguity design
    --the average pooling algorithm now is much more informative and consistent with neighborhood perception.

--- RangeScaling/full_20/trun.py
+++ RangeScaling/full_20/trun.py
@@ -47,9 +47,9 @@ def func(N=None, K=None, state_num=None, generalist_expertise=None, specialist_e
         quantiles = statistics.quantiles(temp_1, n=4)
         first_quantile = quantiles[0]
         last_quantile = quantiles[-1]
-        above_first_quantile = [num for num in temp_1 if num > first_quantile]
+        above_first_quantile = [num for num in temp_1 if num >= first_quantile]
         first_quantile_across_time.append(sum(above_first_quantile) / len(above_first_quantile))
-        below_last_quantile = [num for num in temp_1 if num < last_quantile]
+        below_last_quantile = [num for num in temp_1 if num <= last_quantile]
         last_quantile_across_time.append(sum(below_last_quantile) / len(below_last_quantile))
 
         cog_performance_across_time.append(sum(temp_2) / len(temp_2))
