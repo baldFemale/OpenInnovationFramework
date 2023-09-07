7fd1d3b066736a57b8e7b0fd97f71d56959c5780
毕夏 <978260577@qq.com>
2023-09-07 22:11:33 +0800

    1. Simplify the model, Only two class: --Agent class for G/S/T --Landscape class for 4-state landscape; revise the way to generate landscape, binary first and then quaternary
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --adopt the default element design, resonating with the mindset and cognitive fixation; perhaps removed.
    --adopt the shelter "*" element design, resonating with the myopia induced ambiguity.
    
    3. Introduce the different sub-area in the landscape, removed.
    
    4. Adjust the ambiguity design
    --the average pooling algorithm now is much more informative and consistent with neighborhood perception. removed
    --use the binary-to-quaternary diverging to implement the average pooling reversely
    
    5. Socialization Design
    --Evaluation; Sensitivity: timing, how
    --Sharing; Sensitivity: timing, how (incomplete) -> individual may not provide exact solution of what they are; instead, they may suffer from ambiguity when sharing their solution;
    --It is full of imperfect socialization, such as imperfect sharing (give), imperfect imitation (acquisition), imperfect evaluation;

---
+++ EvaluatingHow/Rollback/G_evaluate_S_2.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+# @Time     : 9/26/2022 20:23
+# @Author   : Junyi
+# @FileName: run.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+import numpy as np
+from Generalist import Generalist
+from Specialist import Specialist
+from Landscape import Landscape
+from Crowd import Crowd
+import multiprocessing as mp
+import time
+from multiprocessing import Semaphore
+import pickle
+
+
+# mp version
+def func(N=None, K=None, state_num=None, generalist_expertise=None, specialist_expertise=None, agent_num=None,
+         search_iteration=None, roll_forward=None, roll_back=None, loop=None, return_dict=None, sema=None):
+    np.random.seed(None)
+    landscape = Landscape(N=N, K=K, state_num=state_num, alpha=0.25)
+    performance_across_agent_time = []
+    cog_performance_across_agent_time = []
+    # Evaluator Crowd
+    crowd = Crowd(N=N, agent_num=50, landscape=landscape, state_num=state_num,
+                           generalist_expertise=12, specialist_expertise=0, label="G")
+    for _ in range(agent_num):
+        specialist = Specialist(N=N, landscape=landscape, state_num=state_num, crowd=crowd,
+                           generalist_expertise=generalist_expertise, specialist_expertise=specialist_expertise)
+        for _ in range(search_iteration):
+            specialist.feedback_search(roll_back_ratio=roll_back, roll_forward_ratio=roll_forward)
+        performance_across_agent_time.append(specialist.fitness_across_time)
+        cog_performance_across_agent_time.append(specialist.cog_fitness_across_time)
+
+    performance_across_time = []
+    cog_performance_across_time = []
+    variance_across_time = []
+    for period in range(search_iteration):
+        temp_1 = [performance_list[period] for performance_list in performance_across_agent_time]
+        temp_2 = [performance_list[period] for performance_list in cog_performance_across_agent_time]
+        performance_across_time.append(sum(temp_1) / len(temp_1))
+        cog_performance_across_time.append(sum(temp_2) / len(temp_2))
+        variance_across_time.append(np.std(temp_1))
+    return_dict[loop] = [performance_across_time, cog_performance_across_time, variance_across_time]
+    sema.release()
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    landscape_iteration = 300
+    agent_num = 100
+    search_iteration = 200
+    N = 9
+    state_num = 4
+    generalist_expertise = 0
+    specialist_expertise = 12
+    K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    # roll_forward_list = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    roll_forward_list = [0.2, 0.3]
+    roll_back_list = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    concurrency = 50
+    # DVs
+    performance_across_K = []
+    variance_across_K = []
+
+    performance_across_K_time = []
+    cog_performance_across_K_time = []
+    variance_across_K_time = []
+    for roll_forward in roll_forward_list:
+        for roll_back in roll_back_list:
+            for K in K_list:
+                manager = mp.Manager()
+                return_dict = manager.dict()
+                sema = Semaphore(concurrency)
+                jobs = []
+                for loop in range(landscape_iteration):
+                    sema.acquire()
+                    p = mp.Process(target=func, args=(N, K, state_num, generalist_expertise, specialist_expertise,
+                                                      agent_num, search_iteration, roll_forward, roll_back, loop, return_dict, sema))
+                    jobs.append(p)
+                    p.start()
+                for proc in jobs:
+                    proc.join()
+                returns = return_dict.values()  # Don't need dict index, since it is repetition.
+
+                temp_fitness_time, temp_cog_time, temp_var_time = [], [], []
+                temp_fitness, temp_cog, temp_var = [], [], []
+                for result in returns:  # 50 landscape repetitions
+                    temp_fitness_time.append(result[0])
+                    temp_cog_time.append(result[1])
+                    temp_var_time.append(result[2])
+
+                    temp_fitness.append(result[0][-1])
+                    temp_cog.append(result[1][-1])
+                    temp_var.append(result[2][-1])
+
+                performance_across_K.append(sum(temp_fitness) / len(temp_fitness))
+                variance_across_K.append(sum(temp_var) / len(temp_var))
+
+                performance_across_time = []
+                cog_performance_across_time = []
+                variance_across_time = []
+                for period in range(search_iteration):
+                    temp_1 = [performance_list[period] for performance_list in temp_fitness_time]
+                    performance_across_time.append(sum(temp_1) / len(temp_1))
+                    temp_2 = [performance_list[period] for performance_list in temp_cog_time]
+                    cog_performance_across_time.append(sum(temp_2) / len(temp_2))
+                    temp_3 = [performance_list[period] for performance_list in temp_var_time]
+                    variance_across_time.append(sum(temp_3) / len(temp_3))
+                performance_across_K_time.append(performance_across_time)
+                cog_performance_across_K_time.append(cog_performance_across_time)
+                variance_across_K_time.append(variance_across_time)
+            # remove time dimension
+            with open("gs_performance_across_K_forward_{0}_backward_{1}".format(roll_forward, roll_back), 'wb') as out_file:
+                pickle.dump(performance_across_K, out_file)
+            with open("gs_variance_across_K_forward_{0}_backward_{1}".format(roll_forward, roll_back), 'wb') as out_file:
+                pickle.dump(variance_across_K, out_file)
+            # retain time dimension
+            with open("gs_performance_across_K_time_forward_{0}_backward_{1}".format(roll_forward, roll_back), 'wb') as out_file:
+                pickle.dump(performance_across_K_time, out_file)
+            with open("gs_cog_performance_across_K_time_forward_{0}_backward_{1}".format(roll_forward, roll_back), 'wb') as out_file:
+                pickle.dump(cog_performance_across_K_time, out_file)
+            with open("gs_variance_across_K_time_forward_{0}_backward_{1}".format(roll_forward, roll_back), 'wb') as out_file:
+                pickle.dump(variance_across_K_time, out_file)
+
+    t1 = time.time()
+    print("Evaluating GS_2: ", time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
+
+
