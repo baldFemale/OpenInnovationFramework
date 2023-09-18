6ed09a2dc95d8dbd97892580c8465f688930d00a
[Junee] <[978260577@qq.com]>
2023-09-19 01:55:00 +0800

    1. Simplify the model, Only two class: --Agent class for G/S/T --Landscape class for 4-state landscape; revise the way to generate landscape, binary first and then quaternary
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --adopt the default element design, resonating with the mindset and cognitive fixation; perhaps removed.
    --adopt the shelter "*" element design, resonating with the myopia induced ambiguity.
    
    3. Introduce the different sub-area in the landscape, removed.
    
    4. Adjust the ambiguity design
    --the average pooling algorithm now is much more informative and consistent with neighborhood perception. removed
    --use the binary-to-quaternary diverging to implement the average pooling reversely
    
    5. Socialization Design
    --Evaluation; Sensitivity: timing, how
    --Sharing; Sensitivity: timing, how (incomplete) -> individual may not provide exact solution of what they are; instead, they may suffer from ambiguity when sharing their solution;
    --It is full of imperfect socialization, such as imperfect sharing (give), imperfect imitation (acquisition), imperfect evaluation;

--- SharingWhen/Sharing/S_send_to_S.py
+++ SharingWhen/Sharing/S_send_to_S.py
@@ -25,7 +25,7 @@ def func(N=None, K=None, state_num=None, generalist_expertise=None, specialist_e
     cog_performance_across_agent_time = []
     # Sharing Crowd
     crowd = Crowd(N=N, agent_num=agent_num, landscape=landscape, state_num=state_num,
-                           generalist_expertise=0, specialist_expertise=12, label="S")
+                           generalist_expertise=0, specialist_expertise=16, label="S")
     for agent in crowd.agents:
         for _ in range(search_iteration):
             agent.search()
@@ -70,12 +70,12 @@ if __name__ == '__main__':
     t0 = time.time()
     landscape_iteration = 300
     agent_num = 100
-    search_iteration = 400
-    N = 9
+    search_iteration = 300
+    N = 12
     state_num = 4
     generalist_expertise = 0
-    specialist_expertise = 12
-    K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    specialist_expertise = 16
+    K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     concurrency = 50
     # DVs
     performance_across_K = []
