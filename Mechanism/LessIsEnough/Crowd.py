3eba30ab2e4e6f41db6a633ba30b1ad75e00c75f
毕夏 <978260577@qq.com>
2023-09-24 22:11:33 +0800

    1. Simplify the model, Three classes: --Generalist --Specialist --Landscape; binary first and then resample into quaternary
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --perceive it as "*" in cognitive fitness -> no difference -> thus feedback from external domain is beneficial
    --calculate it as default -- out of one's control due to unknown
    
    5. Socialization Design
    --Evaluation
    --Sharing;
    ---- individual may not provide exact solution of what they are; instead, they may suffer from ambiguity when sharing their solution; (pending)
    --It is full of imperfect socialization, such as imperfect sharing (give), imperfect imitation (acquisition), imperfect evaluation;
    --First implement the simple model before complicating
    --e.g., agents first all share, and then with probability to share (contingency pending)
    --agents first are all influenced by crowd, and the with probability to be influenced (contingency pending)
    
    6. Measures
    --Average Performance
    --Best/Worst Performance
    --Exact counter of MutualClimb or JointConfusion (pending)

---
+++ Mechanism/LessIsEnough/Crowd.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Time     : 6/22/2023 20:46
+# @Author   : Junyi
+# @FileName: Crowd.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+from Generalist import Generalist
+from Specialist import Specialist
+
+
+class Crowd:
+    def __init__(self, N: int, agent_num: int, generalist_expertise: int, specialist_expertise: int,
+                 landscape: object, state_num: int, label: str):
+        self.agent_num = agent_num
+        self.agents = []
+        for _ in range(agent_num):
+            if label == "G":
+                agent = Generalist(N=N, landscape=landscape, state_num=state_num, generalist_expertise=generalist_expertise)
+                self.agents.append(agent)
+            elif label == "S":
+                agent = Specialist(N=N, landscape=landscape, state_num=state_num, specialist_expertise=specialist_expertise)
+                self.agents.append(agent)
+        self.solutions = []
+
+    def evaluate(self, cur_state: list, next_state: list) -> bool:
+        opinions = [agent.evaluate(cur_state=cur_state,
+                                   next_state=next_state) for agent in self.agents]
+        true_count = sum(1 for item in opinions if item)
+        return true_count > self.agent_num / 2
