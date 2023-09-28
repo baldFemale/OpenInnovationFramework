50055c524c2b255275b2be1cb5c27f3837096caf
毕夏 <978260577@qq.com>
2023-09-28 21:34:37 +0800

    1. Simplify the model, Three classes: --Generalist --Specialist --Landscape; binary first and then resample into quaternary
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --perceive it as "*" in cognitive fitness -> no difference -> thus feedback from external domain is beneficial
    --calculate it as default -- out of one's control due to unknown
    
    5. Socialization Design
    --Evaluation
    --Sharing;
    ---- individual may not provide exact solution of what they are; instead, they may suffer from ambiguity when sharing their solution; (pending)
    --It is full of imperfect socialization, such as imperfect sharing (give), imperfect imitation (acquisition), imperfect evaluation;
    --First implement the simple model before complicating
    --e.g., agents first all share, and then with probability to share (contingency pending)
    --agents first are all influenced by crowd, and the with probability to be influenced (contingency pending)
    
    6. Measures
    --Average Performance
    --Best/Worst Performance
    --Exact counter of MutualClimb or JointConfusion (pending)

--- EvaluatingWhen/EvaluatingExternal/G_evaluate_S.py
+++ EvaluatingWhen/EvaluatingExternal/G_evaluate_S.py
@@ -14,34 +14,37 @@ import time
 from multiprocessing import Semaphore
 import pickle
 
+
 # mp version
-def func(N=None, K=None, state_num=None, specialist_expertise=None, agent_num=None,
+def func(N=None, K=None, state_num=None, individual_expertise=None, crowd_expertise=None, agent_num=None,
          search_iteration=None, loop=None, return_dict=None, sema=None):
     np.random.seed(None)
     landscape = Landscape(N=N, K=K, state_num=state_num, alpha=0.25)
-    performance_across_agent_time = []
-    cog_performance_across_agent_time = []
     # Evaluator Crowd
     crowd = Crowd(N=N, agent_num=50, landscape=landscape, state_num=state_num,
-                           generalist_expertise=12, specialist_expertise=0, label="G")
+                           generalist_expertise=crowd_expertise, specialist_expertise=0, label="G")
+    converged_performance_list = []
+    converged_solution_list = []
     for _ in range(agent_num):
-        specialist = Specialist(N=N, landscape=landscape, state_num=state_num, crowd=crowd, specialist_expertise=specialist_expertise)
+        specialist = Specialist(N=N, landscape=landscape, state_num=state_num, crowd=crowd, specialist_expertise=individual_expertise)
         for _ in range(search_iteration):
             specialist.feedback_search(roll_back_ratio=0.5, roll_forward_ratio=0.5)
-        performance_across_agent_time.append(specialist.fitness_across_time)
-        cog_performance_across_agent_time.append(specialist.cog_fitness_across_time)
-
-    performance_across_time = []
-    cog_performance_across_time = []
-    variance_across_time = []
-    for period in range(search_iteration):
-        temp_1 = [performance_list[period] for performance_list in performance_across_agent_time]
-        temp_2 = [performance_list[period] for performance_list in cog_performance_across_agent_time]
-        performance_across_time.append(sum(temp_1) / len(temp_1))
-        cog_performance_across_time.append(sum(temp_2) / len(temp_2))
-        variance_across_time.append(np.std(temp_1))
-    return_dict[loop] = [performance_across_time, cog_performance_across_time, variance_across_time]
+        converged_performance_list.append(specialist.fitness)
+        converged_solution_list.append(specialist.state)
+    average_performance = sum(converged_performance_list) / len(converged_performance_list)
+    best_performance = max(converged_performance_list)
+    variance = np.std(converged_performance_list)
+    diversity = get_diversity(belief_pool=converged_solution_list)
+    return_dict[loop] = [average_performance, best_performance, variance, diversity]
     sema.release()
+
+def get_diversity(belief_pool: list):
+    unique_solutions = []
+    for belief in belief_pool:
+        string_belief = "".join(belief)
+        unique_solutions.append(string_belief)
+    unique_solutions = set(unique_solutions)
+    return len(unique_solutions)
 
 
 if __name__ == '__main__':
@@ -51,71 +54,53 @@ if __name__ == '__main__':
     search_iteration = 200
     N = 9
     state_num = 4
-    specialist_expertise = 12
+    individual_expertise = 12
+    crowd_knowledge_list = [12, 14, 16, 18]
     # K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     concurrency = 40
-    # DVs
-    performance_across_K = []
-    variance_across_K = []
+    for crowd_knowledge in crowd_knowledge_list:
+        # DVs
+        average_performance_across_K = []
+        best_performance_across_K = []
+        variance_across_K = []
+        diversity_across_K = []
+        for K in K_list:
+            manager = mp.Manager()
+            return_dict = manager.dict()
+            sema = Semaphore(concurrency)
+            jobs = []
+            for loop in range(landscape_iteration):
+                sema.acquire()
+                p = mp.Process(target=func, args=(N, K, state_num, individual_expertise, crowd_knowledge,
+                                                  agent_num, search_iteration, loop, return_dict, sema))
+                jobs.append(p)
+                p.start()
+            for proc in jobs:
+                proc.join()
+            returns = return_dict.values()  # Don't need dict index, since it is repetition.
 
-    performance_across_K_time = []
-    cog_performance_across_K_time = []
-    variance_across_K_time = []
-    for K in K_list:
-        manager = mp.Manager()
-        return_dict = manager.dict()
-        sema = Semaphore(concurrency)
-        jobs = []
-        for loop in range(landscape_iteration):
-            sema.acquire()
-            p = mp.Process(target=func, args=(N, K, state_num, specialist_expertise,
-                                              agent_num, search_iteration, loop, return_dict, sema))
-            jobs.append(p)
-            p.start()
-        for proc in jobs:
-            proc.join()
-        returns = return_dict.values()  # Don't need dict index, since it is repetition.
+            temp_average, temp_best, temp_variance, temp_diversity = [], [], [], []
+            for result in returns:  # 50 landscape repetitions
+                temp_average.append(result[0])
+                temp_best.append(result[1])
+                temp_variance.append(result[2])
+                temp_diversity.append(result[3])
 
-        temp_fitness_time, temp_cog_time, temp_var_time = [], [], []
-        temp_fitness, temp_cog, temp_var = [], [], []
-        for result in returns:  # 50 landscape repetitions
-            temp_fitness_time.append(result[0])
-            temp_cog_time.append(result[1])
-            temp_var_time.append(result[2])
+            average_performance_across_K.append(sum(temp_average) / len(temp_average))
+            best_performance_across_K.append(sum(temp_best) / len(temp_best))
+            variance_across_K.append(sum(temp_variance) / len(temp_variance))
+            diversity_across_K.append(sum(temp_diversity) / len(temp_diversity))
 
-            temp_fitness.append(result[0][-1])
-            temp_cog.append(result[1][-1])
-            temp_var.append(result[2][-1])
-
-        performance_across_K.append(sum(temp_fitness) / len(temp_fitness))
-        variance_across_K.append(sum(temp_var) / len(temp_var))
-
-        performance_across_time = []
-        cog_performance_across_time = []
-        variance_across_time = []
-        for period in range(search_iteration):
-            temp_1 = [performance_list[period] for performance_list in temp_fitness_time]
-            performance_across_time.append(sum(temp_1) / len(temp_1))
-            temp_2 = [performance_list[period] for performance_list in temp_cog_time]
-            cog_performance_across_time.append(sum(temp_2) / len(temp_2))
-            temp_3 = [performance_list[period] for performance_list in temp_var_time]
-            variance_across_time.append(sum(temp_3) / len(temp_3))
-        performance_across_K_time.append(performance_across_time)
-        cog_performance_across_K_time.append(cog_performance_across_time)
-        variance_across_K_time.append(variance_across_time)
-    # remove time dimension
-    with open("gs_performance_across_K", 'wb') as out_file:
-        pickle.dump(performance_across_K, out_file)
-    with open("gs_variance_across_K", 'wb') as out_file:
-        pickle.dump(variance_across_K, out_file)
-    # retain time dimension
-    with open("gs_performance_across_K_time", 'wb') as out_file:
-        pickle.dump(performance_across_K_time, out_file)
-    with open("gs_cog_performance_across_K_time", 'wb') as out_file:
-        pickle.dump(cog_performance_across_K_time, out_file)
-    with open("gs_variance_across_K_time", 'wb') as out_file:
-        pickle.dump(variance_across_K_time, out_file)
+        # remove time dimension
+        with open("gs_performance_across_K_{0}".format(crowd_knowledge), 'wb') as out_file:
+            pickle.dump(average_performance_across_K, out_file)
+        with open("gs_best_performance_across_K_{0}".format(crowd_knowledge), 'wb') as out_file:
+            pickle.dump(best_performance_across_K, out_file)
+        with open("gs_variance_across_K_{0}".format(crowd_knowledge), 'wb') as out_file:
+            pickle.dump(variance_across_K, out_file)
+        with open("gs_diversity_across_K_{0}".format(crowd_knowledge), 'wb') as out_file:
+            pickle.dump(diversity_across_K, out_file)
 
     t1 = time.time()
     print("Evaluating GS: ", time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
