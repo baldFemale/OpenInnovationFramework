a263fef28f1fc0cf97a097aeb272c3486af8fd22
毕夏 <978260577@qq.com>
2023-11-02 20:20:38 +0800

    1. Simplify the model, Three classes: --Generalist --Specialist --Landscape; binary first and then resample into quaternary
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --perceive it as "*" in cognitive fitness -> no difference -> thus feedback from external domain is beneficial
    --calculate it as default -- out of one's control due to unknown
    
    5. Socialization Design
    --Evaluation
    --Sharing;
    ---- individual may not provide exact solution of what they are; instead, they may suffer from ambiguity when sharing their solution; (pending)
    --It is full of imperfect socialization, such as imperfect sharing (give), imperfect imitation (acquisition), imperfect evaluation;
    --First implement the simple model before complicating
    --e.g., agents first all share, and then with probability to share (contingency pending)
    --agents first are all influenced by crowd, and the with probability to be influenced (contingency pending)
    
    6. Measures
    --Average Performance
    --Best/Worst Performance
    --Exact counter of MutualClimb or JointConfusion (pending)
    ----Using procedural vs. discrete mechanism
    ----Procedural: using SuperG and SuperS to show that G are easily get confused and thus their solution retain promising directions; while S are more accurate and thus their solutions are more likely to create joint confusion -> due to the partially attractiveness -> it even become stronger with increasing S knowledge -> One will also get stuck facing the same solution -> it become an paradoxical tension regarding knowledege: more knowledege is good for the focal solver but it is bad for the collaboration

--- Mechanism/LessIsEnough_NotHomo/S_send_to_S_4.py
+++ Mechanism/LessIsEnough_NotHomo/S_send_to_S_4.py
@@ -61,11 +61,11 @@ def func(N=None, K=None, agent_num=None, search_iteration=None, loop=None, retur
 
 if __name__ == '__main__':
     t0 = time.time()
-    landscape_iteration = 400
+    landscape_iteration = 200
     search_iteration = 200
     N = 9
     K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    agent_num_list = np.arange(50, 400, step=50, dtype=int).tolist()
+    agent_num_list = np.arange(1200, 1650, step=50, dtype=int).tolist()
     concurrency = 50
     for agent_num in agent_num_list:
         # DVs
