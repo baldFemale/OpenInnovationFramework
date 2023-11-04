78cb2abab84a156dc19817de05428805b3adcf4e
毕夏 <978260577@qq.com>
2023-11-04 17:31:13 +0800

    1. Simplify the model, Three classes: --Generalist --Specialist --Landscape; binary first and then resample into quaternary
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --perceive it as "*" in cognitive fitness -> no difference -> thus feedback from external domain is beneficial
    --calculate it as default -- out of one's control due to unknown
    
    5. Socialization Design
    --Evaluation
    --Sharing;
    ---- individual may not provide exact solution of what they are; instead, they may suffer from ambiguity when sharing their solution; (pending)
    --It is full of imperfect socialization, such as imperfect sharing (give), imperfect imitation (acquisition), imperfect evaluation;
    --First implement the simple model before complicating
    --e.g., agents first all share, and then with probability to share (contingency pending)
    --agents first are all influenced by crowd, and the with probability to be influenced (contingency pending)
    
    6. Measures
    --Average Performance
    --Best/Worst Performance
    --Exact counter of MutualClimb or JointConfusion (pending)
    ----Using procedural vs. discrete mechanism
    ----Procedural: using SuperG and SuperS to show that G are easily get confused and thus their solution retain promising directions; while S are more accurate and thus their solutions are more likely to create joint confusion -> due to the partially attractiveness -> it even become stronger with increasing S knowledge -> One will also get stuck facing the same solution -> it become an paradoxical tension regarding knowledege: more knowledege is good for the focal solver but it is bad for the collaboration

--- Mechanism/JointSatisfaction_across_K_alpha_0.25/G_evaluate_G.py
+++ Mechanism/JointSatisfaction_across_K_alpha_0.25/G_evaluate_G.py
@@ -16,17 +16,21 @@ import pickle
 
 
 # mp version
-def func(N=None, K=None, state_num=None, generalist_expertise=None, agent_num=None,
+def func(N=None, K=None, agent_num=None,
          search_iteration=None, loop=None, return_dict=None, sema=None):
     np.random.seed(None)
-    landscape = Landscape(N=N, K=K, state_num=state_num, alpha=0.25)
+    landscape = Landscape(N=N, K=K, state_num=4, alpha=0.25)
     # Sender Crowd
-    crowd = Crowd(N=N, agent_num=state_num, landscape=landscape, state_num=state_num,
+    crowd = Crowd(N=N, agent_num=agent_num, landscape=landscape, state_num=4,
                            generalist_expertise=12, specialist_expertise=0, label="G")
+    for agent in crowd.agents:
+        for _ in range(search_iteration):
+            agent.search()
     joint_confusion_rate_list = []
+    # Receiver Repetition
     for _ in range(agent_num):
         # Focal Agent
-        generalist = Generalist(N=N, landscape=landscape, state_num=state_num, crowd=crowd, generalist_expertise=generalist_expertise)
+        generalist = Generalist(N=N, landscape=landscape, state_num=4, crowd=crowd, generalist_expertise=12)
         for _ in range(search_iteration):
             generalist.search()
         # Joint satisfaction
@@ -37,12 +41,11 @@ def func(N=None, K=None, state_num=None, generalist_expertise=None, agent_num=No
             learnt_solution = agent.state.copy()
             for index in sender_domain:
                 learnt_solution[index] = sender_solution[index]
-            suggestions = agent.suggest_better_state_from_expertise(state=learnt_solution)
-            for each_suggestion in suggestions:
-                climbs = generalist.suggest_better_state_from_expertise(state=each_suggestion)
-                climbs.remove(reached_solution)
-                count += len(climbs)
-        joint_confusion_rate = count / state_num
+            cog_learnt_solution = agent.state_2_cog_state(state=learnt_solution)
+            cog_learnt_fitness = agent.get_cog_fitness(cog_state=cog_learnt_solution, state=learnt_solution)
+            if cog_learnt_fitness > agent.cog_fitness:
+                count += 1
+        joint_confusion_rate = count / agent_num
         joint_confusion_rate_list.append(joint_confusion_rate)
     final_joint_confusion_rate = sum(joint_confusion_rate_list) / len(joint_confusion_rate_list)
     return_dict[loop] = [final_joint_confusion_rate]
@@ -55,9 +58,6 @@ if __name__ == '__main__':
     agent_num = 500
     search_iteration = 200
     N = 9
-    state_num = 4
-    generalist_expertise = 12
-    # K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     concurrency = 40
     # DVs
@@ -69,8 +69,7 @@ if __name__ == '__main__':
         jobs = []
         for loop in range(landscape_iteration):
             sema.acquire()
-            p = mp.Process(target=func, args=(N, K, state_num, generalist_expertise,
-                                              agent_num, search_iteration, loop, return_dict, sema))
+            p = mp.Process(target=func, args=(N, K, agent_num, search_iteration, loop, return_dict, sema))
             jobs.append(p)
             p.start()
         for proc in jobs:
@@ -84,9 +83,9 @@ if __name__ == '__main__':
         joint_confusion_across_K.append(sum(temp_joint_confusion) / len(temp_joint_confusion))
 
     # remove time dimension
-    with open("gg_joint_confusion_across_K", 'wb') as out_file:
+    with open("gg_joint_satisfaction_across_K", 'wb') as out_file:
         pickle.dump(joint_confusion_across_K, out_file)
     t1 = time.time()
-    print("Evaluating GG: ", time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
+    print("Joint Satisfaction GG: ", time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
 
 
