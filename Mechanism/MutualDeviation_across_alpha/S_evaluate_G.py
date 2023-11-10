2886ed8f8d1b4d46dd2c8ed5783e7eed6b8fc478
毕夏 <978260577@qq.com>
2023-11-10 21:02:37 +0800

    1. Simplify the model, Three classes: --Generalist --Specialist --Landscape; binary first and then resample into quaternary
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --perceive it as "*" in cognitive fitness -> no difference -> thus feedback from external domain is beneficial
    --calculate it as default -- out of one's control due to unknown
    
    5. Socialization Design
    --Evaluation
    --Sharing;
    ---- individual may not provide exact solution of what they are; instead, they may suffer from ambiguity when sharing their solution; (pending)
    --It is full of imperfect socialization, such as imperfect sharing (give), imperfect imitation (acquisition), imperfect evaluation;
    --First implement the simple model before complicating
    --e.g., agents first all share, and then with probability to share (contingency pending)
    --agents first are all influenced by crowd, and the with probability to be influenced (contingency pending)
    
    6. Measures
    --Average Performance
    --Best/Worst Performance
    --Exact counter of MutualClimb or JointConfusion (pending)
    ----Using procedural vs. discrete mechanism
    ----Procedural: using SuperG and SuperS to show that G are easily get confused and thus their solution retain promising directions; while S are more accurate and thus their solutions are more likely to create joint confusion -> due to the partially attractiveness -> it even become stronger with increasing S knowledge -> One will also get stuck facing the same solution -> it become an paradoxical tension regarding knowledege: more knowledege is good for the focal solver but it is bad for the collaboration

---
+++ Mechanism/MutualDeviation_across_alpha/S_evaluate_G.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+# @Time     : 9/26/2022 20:23
+# @Author   : Junyi
+# @FileName: run.py
+# @Software  : PyCharm
+# Observing PEP 8 coding style
+import numpy as np
+from Generalist import Generalist
+from Specialist import Specialist
+from Landscape import Landscape
+from Crowd import Crowd
+import multiprocessing as mp
+import time
+from multiprocessing import Semaphore
+import pickle
+
+
+# mp version
+def func(N=None, K=None, state_num=None, agent_num=None,
+         search_iteration=None, loop=None, return_dict=None, sema=None):
+    np.random.seed(None)
+    landscape = Landscape(N=N, K=K, state_num=state_num, alpha=0.25)
+    # Evaluator Crowd
+    crowd = Crowd(N=N, agent_num=agent_num, landscape=landscape, state_num=state_num,
+                           generalist_expertise=0, specialist_expertise=12, label="S")
+    mutual_climb_rate_list = []
+    for _ in range(agent_num):
+        generalist = Generalist(N=N, landscape=landscape, state_num=state_num, crowd=crowd, generalist_expertise=12)
+        for _ in range(search_iteration):
+            generalist.search()
+        # Mutual Climb
+        reached_solution = generalist.state.copy()
+        count = 0
+        for agent in crowd.agents:
+            suggestions = agent.suggest_better_state_from_expertise(state=reached_solution)
+            for each_suggestion in suggestions:
+                climbs = generalist.suggest_better_state_from_expertise(state=each_suggestion)
+                if reached_solution in climbs:
+                    climbs.remove(reached_solution)
+                count += len(climbs)
+            if len(suggestions) != 0:
+                count /= len(suggestions)
+        mutual_climb_rate = count / agent_num
+        mutual_climb_rate_list.append(mutual_climb_rate)
+    final_mutual_climb_rate = sum(mutual_climb_rate_list) / len(mutual_climb_rate_list)
+    return_dict[loop] = [final_mutual_climb_rate]
+    sema.release()
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    landscape_iteration = 200
+    agent_num = 500
+    search_iteration = 200
+    N = 9
+    state_num = 4
+    K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    concurrency = 100
+    # DVs
+    joint_confusion_across_K = []
+    for K in K_list:
+        manager = mp.Manager()
+        return_dict = manager.dict()
+        sema = Semaphore(concurrency)
+        jobs = []
+        for loop in range(landscape_iteration):
+            sema.acquire()
+            p = mp.Process(target=func, args=(N, K, state_num, agent_num, search_iteration, loop, return_dict, sema))
+            jobs.append(p)
+            p.start()
+        for proc in jobs:
+            proc.join()
+        returns = return_dict.values()  # Don't need dict index, since it is repetition.
+
+        temp_joint_confusion = []
+        for result in returns:  # 50 landscape repetitions
+            temp_joint_confusion.append(result[0])
+
+        joint_confusion_across_K.append(sum(temp_joint_confusion) / len(temp_joint_confusion))
+
+    # remove time dimension
+    with open("sg_mutual_deviation_across_K", 'wb') as out_file:
+        pickle.dump(joint_confusion_across_K, out_file)
+
+    t1 = time.time()
+    print("SG: ", time.strftime("%H:%M:%S", time.gmtime(t1-t0)))
