c1410a948ec7eaef760d691ff1bc767adb063c04
毕夏 <978260577@qq.com>
2023-11-10 21:46:57 +0800

    1. Simplify the model, Three classes: --Generalist --Specialist --Landscape; binary first and then resample into quaternary
    2. Agents all search on the finest landscape, just perceiving it from different perspectives.
    --perceive it as "*" in cognitive fitness -> no difference -> thus feedback from external domain is beneficial
    --calculate it as default -- out of one's control due to unknown
    
    5. Socialization Design
    --Evaluation
    --Sharing;
    ---- individual may not provide exact solution of what they are; instead, they may suffer from ambiguity when sharing their solution; (pending)
    --It is full of imperfect socialization, such as imperfect sharing (give), imperfect imitation (acquisition), imperfect evaluation;
    --First implement the simple model before complicating
    --e.g., agents first all share, and then with probability to share (contingency pending)
    --agents first are all influenced by crowd, and the with probability to be influenced (contingency pending)
    
    6. Measures
    --Average Performance
    --Best/Worst Performance
    --Exact counter of MutualClimb or JointConfusion (pending)
    ----Using procedural vs. discrete mechanism
    ----Procedural: using SuperG and SuperS to show that G are easily get confused and thus their solution retain promising directions; while S are more accurate and thus their solutions are more likely to create joint confusion -> due to the partially attractiveness -> it even become stronger with increasing S knowledge -> One will also get stuck facing the same solution -> it become an paradoxical tension regarding knowledege: more knowledege is good for the focal solver but it is bad for the collaboration

--- Mechanism/JointSatisfaction_across_alpha/S_evaluate_G_1.py
+++ Mechanism/JointSatisfaction_across_alpha/S_evaluate_G_1.py
@@ -56,14 +56,14 @@ if __name__ == '__main__':
     now = datetime.datetime.now()
     print(now.strftime("%Y-%m-%d %H:%M:%S"))
     t0 = time.time()
-    landscape_iteration = 400
+    landscape_iteration = 200
     agent_num = 500
     search_iteration = 200
     N = 9
     K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     # alpha_list = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45]
     alpha_list = [0.05, 0.10, 0.15, 0.20]
-    concurrency = 100
+    concurrency = 50
     # DVs
     for alpha in alpha_list:
         joint_confusion_across_K = []
