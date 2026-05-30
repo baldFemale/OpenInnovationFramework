045b96acfc2e66c0f7d0f09df49dcdc51bd159b0
Junyi Li <978260577@qq.com>
2026-05-30 19:36:18 +0800

    Manuscript preparing 1. Formating figures 2. Organize experiments and flow 2. Calibrate the interaction setting

--- ISR/2_MoreThanParallel/Timing/S_send_to_S_visibility_timing.py
+++ ISR/2_MoreThanParallel/Timing/S_send_to_S_visibility_timing.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     now = datetime.datetime.now()
     print(now.strftime("%Y-%m-%d %H:%M:%S"))
     t0 = time.time()
-    landscape_iteration = 400
+    landscape_iteration = 200
     search_iteration = 200
     N = 9
     K_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
