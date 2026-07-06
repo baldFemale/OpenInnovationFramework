b6fb61a7ce3d948fedf09de0920b14bccb488082
Junyi Li <978260577@qq.com>
2026-07-06 20:34:06 +0800

    1st revision: crowd separation 2nd revision: partial visibility -> full visibility 3rd revision: improve solver knowledge (pending) 4th revision: fix visible solvers across time; add pairwise diversity; default visibility interval; 5th revision: add a performance-based visibility 6th revision: add a High-Medium-Low knowledge Experiment (Medium changed to G18-S16) 7th revision (**pending**): add the information cascade assumption; remove the separation between senders and receivers.

--- ISR/2_MoreThanParallel/Performance_ranked/G_send_to_G_rank_visibility.py
+++ ISR/2_MoreThanParallel/Performance_ranked/G_send_to_G_rank_visibility.py
@@ -147,13 +147,13 @@ if __name__ == '__main__':
     print(now.strftime("%Y-%m-%d %H:%M:%S"))
     t0 = time.time()
 
-    landscape_iteration = 200
+    landscape_iteration = 300
     search_iteration = 300
     N = 9
     K_list = [1, 2, 3, 4, 5, 6, 7, 8]
 
     # Visibility degree is the focal running parameter.
-    visibility_prob_list = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5,
+    visibility_prob_list = [0.0, 0.005, 0.01, 0.02, 0.04, 0.08, 0.1, 0.2, 0.3, 0.4, 0.5,
                             0.6, 0.7, 0.8, 0.9, 1.0]
 
     # Visibility interval: how frequently visibility is activated.
